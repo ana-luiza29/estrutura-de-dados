d1304c5a1bb6082bcdc52f2f99c8de36661e778b
ana-luiza29 <freitas.analuiza29@gmail.com>
2024-05-06 09:56:59 -0400

    exemplo de lasindexOf

---
+++ array/indexOf.js
@@ -0,0 +1,4 @@
+let array = [1,2,3,4,5];
+let index = array.indexOf(3);
+console.log(index);
+//saida: 2 (indice onde 3 esta localizado)
---
+++ array/lastindexOf.js
@@ -0,0 +1,4 @@
+let array = [1,2,3,4,5,3];
+let lastIndex = array.lastIndexOf(3);
+console.log(lastIndex);
+//saida:5 (ultimo indice onde 3 esta localizado)
