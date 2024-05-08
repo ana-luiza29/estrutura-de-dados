e46ffd70b8bf7164ecf3ac51d6135f5a75fa6b81
ana-luiza29 <freitas.analuiza29@gmail.com>
2024-05-08 09:23:29 -0400

    object HasOwn

---
+++ objetos/objectHasOwnPrioperty.js
@@ -0,0 +1,8 @@
+const person = {
+    name: "ana",
+    age: "16",
+    city: "cuiaba",
+};
+console.log(person.hasOwnProperty('name')); // Siada: true
+console.log(personhasOwnProperty('toString'));
+//Saida: False (toString e uma propriedade herdada)
