74a8d283b823f47779937987e72b07a50ea7f06e
ana-luiza29 <freitas.analuiza29@gmail.com>
2024-05-08 09:00:38 -0400

    object Entries

---
+++ objetos/objectEntries.js
@@ -0,0 +1,9 @@
+const person = {
+    name: "Ana",
+    age: "16",
+    city: "cuiaba",
+};
+
+const entries = Object.entries(person);
+console.log(entries);
+//saida: [ [ 'name', 'Ana' ], [ 'age', '16' ], [ 'city', 'cuiaba' ] ]
